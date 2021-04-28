212138f0994c18b7750f9a760bf0b90ef28cbd7f
Makoto Kinoshita <mkinoshi12@gmail.com>
2021-04-28 17:45:32 +0900

    Finished the initial version

---
+++ README.md
@@ -0,0 +1,78 @@
+# Rpilot
+
+Rpilot is a CLI tool for managing env files. You can use multiple env files for one project with one command. It is similar to how you can have multiple profiles for AWS CLI tool.
+
+## Before using this
+
+This CLI will generate two files. Please add them in `gitignore` of the project.
+
+```
+.env
+.rpilot
+```
+
+You also need administrative permissions for `apply` and `edit` commands.
+
+## Installation
+
+More details to come.
+
+## Usage Examples
+
+- Initialize Rpilot for your project
+
+```
+rpilot init
+```
+
+- Add a new env profile
+
+```
+rpilot new --name default
+```
+
+- Apply the specific profile
+
+```
+rpilot apply --name default
+```
+
+- List all of the available profiles
+
+```
+rpilot list
+```
+
+- Edit a specific profile
+
+```
+rpilot edit --name default
+```
+
+- Remove a specific profile
+
+```
+rpilot remove --name default
+```
+
+- Show the env variables for a specific profile
+
+```
+rpilot show --name default
+```
+
+- Get the current profile
+
+```
+rpilot current
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate. Enjoy!
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,41 @@
-use exitfailure::ExitFailure;
+use env_logger::Env;
 use structopt::StructOpt;
 
 pub mod commands;
 pub mod common;
-use commands::new::{execute, NewCommmand};
-use commands::init::{execute, InitCommand};
+use commands::apply::{execute as apply, ApplyCommand};
+use commands::current::execute as current;
+use commands::edit::{execute as edit, EditCommand};
+use commands::init::execute as init;
+use commands::list::execute as list;
+use commands::new::{execute as new, NewCommand};
+use commands::remove::{execute as remove, RemoveCommand};
+use commands::show::{execute as show, ShowCommand};
 
 #[derive(Debug, PartialEq, StructOpt)]
 enum Rpilot {
-    New(NewCommmand),
-    Init(InitCommand)
+    New(NewCommand),
+    Init,
+    List,
+    Current,
+    Remove(RemoveCommand),
+    Show(ShowCommand),
+    Edit(EditCommand),
+    Apply(ApplyCommand),
 }
 
-fn main() -> Result<(), ExitFailure> {
-    let args = Rpilot::from_args();
-    match args {
-        Rpilot::New(v) => execute(&v),
-        _ => {
-            panic!("Unmatching command");
-        }
+fn main() {
+    let env = Env::new().filter_or("LOG", "info");
+    env_logger::init_from_env(env);
+
+    match Rpilot::from_args() {
+        Rpilot::New(v) => new(&v),
+        Rpilot::Init => init(),
+        Rpilot::List => list(),
+        Rpilot::Current => current(),
+        Rpilot::Remove(v) => remove(&v),
+        Rpilot::Show(v) => show(&v),
+        Rpilot::Edit(v) => edit(&v),
+        Rpilot::Apply(v) => apply(&v),
     };
-    Ok(())
 }
